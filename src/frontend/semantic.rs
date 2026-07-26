7994dc76fef2f22f73cb191ea8316e495bee2387
Alexander Vinarsky <vinarskydichtv@gmail.com>
2026-07-26 23:58:54 +0300

    semantic analysis update

--- src/frontend/semantic.rs
+++ src/frontend/semantic.rs
@@ -1,4 +1,4 @@
-use super::ast::{Function, Item, Program};
+use super::ast::{Block, Function, Item, Program};
 use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -7,53 +7,50 @@ pub struct SemanticError {
 }
 
 pub struct SemanticAnalyzer {
-    functions: HashSet<String>,
+    functions: HashMap<String, usize>,
 }
 
 impl SemanticAnalyzer {
     pub fn new() -> Self {
         Self {
-            functions: HashSet::new(),
+            functions: HashMap::new(),
         }
     }
 
     pub fn analyze_program(&mut self, program: &Program) -> Result<(), SemanticError> {
+        self.collect_function_signatures(program)?;
         for item in &program.items {
             self.analyze_item(item)?;
         }
-
         Ok(())
     }
 
     fn analyze_item(&mut self, item: &Item) -> Result<(), SemanticError> {
         match item {
             Item::Function(function) => {
-                if self.functions.contains(&function.name) {
-                    return Err(SemanticError {
-                        message: format!("Duplicate function name: {}", function.name),
-                    });
-                }
-                self.functions.insert(function.name.clone());
+                self.analyze_function(function)?;
                 Ok(())
             }
         }
     }
 
     fn register_function(&mut self, function: &Function) -> Result<(), SemanticError> {
-        if self.functions.contains(&function.name) {
+        let old_value = self
+            .functions
+            .insert(function.name.clone(), function.params.len());
+        if old_value.is_some() {
             return Err(SemanticError {
                 message: format!("duplicate function name: {}", function.name),
             });
         }
-        self.functions.insert(function.name.clone());
         Ok(())
     }
 
     fn analyze_function(&mut self, function: &Function) -> Result<(), SemanticError> {
         self.check_duplicate_params(function)?;
+        self.analyze_block(&function.body)?;
         Ok(())
     }
-
     fn check_duplicate_params(&self, function: &Function) -> Result<(), SemanticError> {
         let mut params = HashSet::new();
         for param in &function.params {
@@ -66,6 +63,23 @@ impl SemanticAnalyzer {
                 });
             }
             params.insert(param.name.clone());
+        }
+        Ok(())
+    }
+
+    fn analyze_block(&mut self, block: &Block) -> Result<(), SemanticError> {
+        for stmt in &block.stmts {
+            //self.analyze_stmt(stmt)?;
+        }
+        Ok(())
+    }
+    fn collect_function_signatures(&mut self, program: &Program) -> Result<(), SemanticError> {
+        for item in &program.items {
+            match item {
+                Item::Function(function) => {
+                    self.register_function(function)?;
+                }
+            }
         }
         Ok(())
     }
