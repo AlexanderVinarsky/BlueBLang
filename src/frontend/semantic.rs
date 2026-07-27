3e35046309c28e33c85933d2367cab668949354c
Alexander Vinarsky <vinarskydichtv@gmail.com>
2026-07-27 23:57:00 +0300

    semantic tests update

--- src/frontend/semantic.rs
+++ src/frontend/semantic.rs
@@ -1,4 +1,4 @@
-use super::ast::{Block, Function, Item, Program};
+use super::ast::{Block, Expr, Function, Item, Program, Stmt};
 use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -17,6 +17,24 @@ impl SemanticAnalyzer {
         }
     }
 
+    fn collect_function_signatures(&mut self, program: &Program) -> Result<(), SemanticError> {
+        for item in &program.items {
+            match item {
+                Item::Function(function) => {
+                    self.register_function(function)?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    // analyze_program
+    //  -> analyze_item
+    //      -> analyze_function
+    //          -> analyze_block
+    //              -> analyze_stmt
+    //                  -> analyze_expr
+
     pub fn analyze_program(&mut self, program: &Program) -> Result<(), SemanticError> {
         self.collect_function_signatures(program)?;
         for item in &program.items {
@@ -32,6 +50,127 @@ impl SemanticAnalyzer {
                 Ok(())
             }
         }
+    }
+
+    fn analyze_block(&mut self, block: &Block) -> Result<(), SemanticError> {
+        for stmt in &block.stmts {
+            self.analyze_stmt(stmt)?;
+        }
+        Ok(())
+    }
+
+    fn analyze_stmt(&mut self, stmt: &Stmt) -> Result<(), SemanticError> {
+        match stmt {
+            Stmt::Let { value, .. } => {
+                self.analyze_expr(value)?;
+                Ok(())
+            }
+
+            Stmt::ExprStmt(expr) => {
+                self.analyze_expr(expr)?;
+                Ok(())
+            }
+
+            Stmt::Return(expr) => {
+                if let Some(expr) = expr {
+                    self.analyze_expr(expr)?;
+                }
+                Ok(())
+            }
+
+            Stmt::Assign { target, value } => {
+                self.analyze_expr(target)?;
+                self.analyze_expr(value)?;
+                Ok(())
+            }
+
+            Stmt::Block(block) => {
+                self.analyze_block(block)?;
+                Ok(())
+            }
+
+            Stmt::If {
+                condition,
+                then_branch,
+                else_branch,
+            } => {
+                self.analyze_expr(condition)?;
+                self.analyze_stmt(then_branch.as_ref())?;
+
+                if let Some(else_branch) = else_branch {
+                    self.analyze_stmt(else_branch.as_ref())?;
+                }
+                Ok(())
+            }
+
+            Stmt::While { condition, body } => {
+                self.analyze_expr(condition)?;
+                self.analyze_stmt(body.as_ref())?;
+
+                Ok(())
+            }
+        }
+    }
+
+    fn analyze_expr(&mut self, expr: &Expr) -> Result<(), SemanticError> {
+        match expr {
+            Expr::Call { callee, args } => {
+                self.analyze_expr(callee)?;
+                for arg in args {
+                    self.analyze_expr(arg)?;
+                }
+                if let Expr::Identifier(name) = callee.as_ref() {
+                    self.check_function_call(name, args.len())?;
+                }
+                Ok(())
+            }
+            Expr::Binary { left, right, .. } => {
+                self.analyze_expr(left)?;
+                self.analyze_expr(right)?;
+                Ok(())
+            }
+            Expr::Unary { expr, .. } => {
+                self.analyze_expr(expr)?;
+                Ok(())
+            }
+            Expr::Member { object, .. } => {
+                self.analyze_expr(object)?;
+                Ok(())
+            }
+            Expr::Index { object, index } => {
+                self.analyze_expr(object)?;
+                self.analyze_expr(index)?;
+                Ok(())
+            }
+            Expr::Identifier(_) => Ok(()),
+            _ => Ok(()),
+        }
+    }
+
+    fn check_function_call(
+        &self,
+        name: &str,
+        actual_arg_count: usize,
+    ) -> Result<(), SemanticError> {
+        let expected_arg_count = match self.functions.get(name) {
+            Some(count) => *count,
+            None => {
+                return Err(SemanticError {
+                    message: format!("unknown function: {}", name),
+                });
+            }
+        };
+
+        if expected_arg_count != actual_arg_count {
+            return Err(SemanticError {
+                message: format!(
+                    "Wrong number of arguments for function `{}`. Expected {}, got {}",
+                    name, expected_arg_count, actual_arg_count
+                ),
+            });
+        }
+
+        Ok(())
     }
 
     fn register_function(&mut self, function: &Function) -> Result<(), SemanticError> {
@@ -51,35 +190,19 @@ impl SemanticAnalyzer {
         self.analyze_block(&function.body)?;
         Ok(())
     }
+
     fn check_duplicate_params(&self, function: &Function) -> Result<(), SemanticError> {
         let mut params = HashSet::new();
         for param in &function.params {
             if params.contains(&param.name) {
                 return Err(SemanticError {
                     message: format!(
-                        "duplicate parameter name `{}` in function `{}`",
+                        "Duplicate parameter name `{}` in function `{}`",
                         param.name, function.name
                     ),
                 });
             }
             params.insert(param.name.clone());
-        }
-        Ok(())
-    }
-
-    fn analyze_block(&mut self, block: &Block) -> Result<(), SemanticError> {
-        for stmt in &block.stmts {
-            //self.analyze_stmt(stmt)?;
-        }
-        Ok(())
-    }
-    fn collect_function_signatures(&mut self, program: &Program) -> Result<(), SemanticError> {
-        for item in &program.items {
-            match item {
-                Item::Function(function) => {
-                    self.register_function(function)?;
-                }
-            }
         }
         Ok(())
     }
